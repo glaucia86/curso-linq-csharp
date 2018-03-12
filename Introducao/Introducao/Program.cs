725d7cc18aedb3ed488570e44a05e88b5854eacc
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-03-12 17:09:04 -0300

    Exemplo de consultas simples e complexas (Select) com linq.

---
+++ Introducao/Introducao/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introducao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LinqSimples();
+            LinqComplexo();
+        }
+
+        private static void LinqSimples()
+        {
+            int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            var numerosFiltrados = numeros.Where(x => x > 4);
+
+            foreach (var item in numerosFiltrados)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void LinqComplexo()
+        {
+            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var resultado = from num in numeros
+                            where num > 4
+                            select num;
+
+            foreach (var item in resultado)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
