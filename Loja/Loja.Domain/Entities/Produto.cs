5bd84c45aedccd7826371acb6b25f811cdfd2719
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-03-13 12:40:29 -0300

    Exercícios relacionados a uso de objetos complexos com Linq

--- Loja/Loja.Domain/Entities/Produto.cs
+++ Loja/Loja.Domain/Entities/Produto.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Loja.Domain.Entities
 {
     public class Produto
     {
+        private List<Produto> _produtos;
+
+        public Produto()
+        {
+            _produtos = new List<Produto>();    
+        }
+
+        //Método para Listar Produtos:
+        public List<Produto> ListarProdutos()
+        {
+            _produtos.Add(new Produto() { IdProduto = Guid.NewGuid(), Nome = "Banana", Quantidade = 2, Valor = 5, DataVencimento = DateTime.Now.AddDays(5) });
+            _produtos.Add(new Produto() { IdProduto = Guid.NewGuid(), Nome = "Maçã", Quantidade = 10, Valor = 15, DataVencimento = DateTime.Now.AddDays(3) });
+            _produtos.Add(new Produto() { IdProduto = Guid.NewGuid(), Nome = "Pêra", Quantidade = 3, Valor = 23, DataVencimento = DateTime.Now.AddDays(4) });
+            _produtos.Add(new Produto() { IdProduto = Guid.NewGuid(), Nome = "Melancia", Quantidade = 1, Valor = 8, DataVencimento = DateTime.Now.AddDays(6) });
+            _produtos.Add(new Produto() { IdProduto = Guid.NewGuid(), Nome = "Laranja", Quantidade = 12, Valor = 11, DataVencimento = DateTime.Now.AddDays(2) });
+            _produtos.Add(new Produto() { IdProduto = Guid.NewGuid(), Nome = "Manga", Quantidade = 6, Valor = 3, DataVencimento = DateTime.Now.AddDays(1) });
+            _produtos.Add(new Produto() { IdProduto = Guid.NewGuid(), Nome = "Coco", Quantidade = 3, Valor = 9, DataVencimento = DateTime.Now.AddDays(2) });
+
+            return _produtos;
+        }
+
         public Guid IdProduto { get; set; }
 
         public string Nome { get; set; }
@@ -13,5 +35,6 @@ namespace Loja.Domain.Entities
         public DateTime DataVencimento { get; set; }
 
         public int Quantidade { get; set; }
+
     }
 }
--- Loja/Loja/Program.cs
+++ Loja/Loja/Program.cs
@@ -1,15 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Loja.Domain.Entities;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Loja
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            //LinqComplexo();
+            LinqSimples();
+        }
+
+        private static void LinqComplexo()
+        {
+            var produto = new Produto();
+
+            var produtos = produto.ListarProdutos()
+                .Where(x => x.Valor > 4)
+                .OrderBy(x => x.Nome);
+
+            foreach (var item in produtos)
+            {
+                Console.WriteLine(item.Nome);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void LinqSimples()
+        {
+            new Produto()
+                .ListarProdutos()
+                .Where(x => x.Valor > 4)
+                .ToList()
+                .ForEach(item =>
+                {
+                    Console.WriteLine(item.Nome);
+                    Console.WriteLine(item.Valor);
+                });
+
+            Console.ReadKey();
         }
     }
 }
