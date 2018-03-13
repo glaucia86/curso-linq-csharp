d5e7ed87dcb0c64d75bfec546b92a14ffde1acd2
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-03-13 11:55:13 -0300

    Criação do arquivo 'Produto.cs'

--- Loja/Loja.Domain/Entities/Produto.cs
+++ Loja/Loja.Domain/Entities/Produto.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Loja.Domain.Entities
 {
     public class Produto
     {
+        public Guid IdProduto { get; set; }
+
+        public string Nome { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public DateTime DataVencimento { get; set; }
+
+        public int Quantidade { get; set; }
     }
 }
